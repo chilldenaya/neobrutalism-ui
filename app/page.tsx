a6e735a67858d710311ed7229460fbfb5a6e76ac
denaya <chilldenaya@gmail.com>
2024-04-03 22:47:48 +0800

    feat: :sparkles: show UI demo on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,52 @@
+"use client";
+
 import Button from "./components/Button";
 import Card from "./components/Card";
 
+const cardData = [
+  {
+    image: "/assets/1.jpeg",
+    title: "Neo Brutalism",
+    description:
+      "Neobrutalism is an aesthetic characterized by high contrast elements, bright colors, and bold shapes. It is often used to make a statement, as it is meant to be eye-catching and stand out to the viewer. It is also used in user interface and web design, with features such as vibrant colors and bold shapes. Neobrutalism can also be used in print design, with an example being a bold shape with a vibrant color fill applied to it.",
+  },
+  {
+    image: "/assets/2.jpeg",
+    title: "Neo Brutalism",
+    description:
+      "It is often used to make a statement, as it is meant to be eye-catching and stand out to the viewer. It is also used in user interface and web design, with features such as vibrant colors and bold shapes. Neobrutalism can also be used in print design, with an example being a bold shape with a vibrant color fill applied to it.",
+  },
+  {
+    image: "/assets/3.jpeg",
+    title: "Neo Brutalism",
+    description:
+      "It is also used in user interface and web design, with features such as vibrant colors and bold shapes. Neobrutalism can also be used in print design, with an example being a bold shape with a vibrant color fill applied to it.",
+  },
+];
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold font-mono">Hello, world!</h1>
-      <div className="flex flex-col justify-center items-center">
-        <p className="text-3xl italic font-pen">
-          We are extremely proud and protective of our active & thriving
-          community.
-        </p>
-        <div className="flex flex-row gap-12">
-          <Button className="hover-3d bg-lime-100 items-center rounded-md m-2 p-2">
-            Ask Me Anything
-          </Button>
-          <Button className="hover-3d bg-lime-100 items-center rounded-md m-2 p-2">
-            Learn More...
-          </Button>
+    <main className="flex min-h-screen flex-col items-left justify-between p-24 gap-12">
+      <h1 className="text-4xl font-bold font-mono">Neo Brutalism UI ✨</h1>
+      <div className="flex flex-col justify-center items-left">
+        <p className="text-4xl font-mono">Button</p>
+        <div className="flex flex-row gap-2">
+          <Button>Click Me!</Button>
+          <Button>Satisfying!</Button>
         </div>
       </div>
-      <div className="flex flex-row gap-12">
-        <Card
-          title="Neo Brutallism"
-          description="Neobrutalism is an aesthetic characterized by high contrast
-      elements, bright colors, and bold shapes. It is often used to
-      make a statement, as it is meant to be eye-catching and stand
-      out to the viewer. It is also used in user interface and web
-      design, with features such as vibrant colors and bold shapes.
-      Neobrutalism can also be used in print design, with an example
-      being a bold shape with a vibrant color fill applied to it."
-        />
-        <Card
-          title="Neo Brutallism"
-          description="Neobrutalism is an aesthetic characterized by high contrast
-      elements, bright colors, and bold shapes. It is often used to
-      make a statement, as it is meant to be eye-catching and stand
-      out to the viewer. It is also used in user interface and web
-      design, with features such as vibrant colors and bold shapes.
-      Neobrutalism can also be used in print design, with an example
-      being a bold shape with a vibrant color fill applied to it."
-        />
-        <Card
-          title="Neo Brutallism"
-          description="Neobrutalism is an aesthetic characterized by high contrast
-      elements, bright colors, and bold shapes. It is often used to
-      make a statement, as it is meant to be eye-catching and stand
-      out to the viewer. It is also used in user interface and web
-      design, with features such as vibrant colors and bold shapes.
-      Neobrutalism can also be used in print design, with an example
-      being a bold shape with a vibrant color fill applied to it."
-        />
+      <div className="flex flex-col gap-2">
+        <p className="text-4xl font-mono">Card</p>
+        <div className="flex flex-row items-center gap-6">
+          {cardData.map((card, index) => (
+            <Card
+              key={index}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
