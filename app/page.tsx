2d8421f0d3d37e84b9cd63d0280bda2085be126e
denaya <chilldenaya@gmail.com>
2024-04-03 20:36:26 +0800

    feat: :sparkles: add card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
+import Card from "./components/Card";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold font-pen">Hello, world!</h1>
-      <p className="text-lg font-bold italic font-mono">
+      <h1 className="text-4xl font-bold font-mono">Hello, world!</h1>
+      <p className="text-3xl italic font-pen">
         We are extremely proud and protective of our active & thriving
         community.
       </p>
+      <div className="flex flex-row gap-12">
+        <Card
+          title="Neo Brutallism"
+          description="Neobrutalism is an aesthetic characterized by high contrast
+      elements, bright colors, and bold shapes. It is often used to
+      make a statement, as it is meant to be eye-catching and stand
+      out to the viewer. It is also used in user interface and web
+      design, with features such as vibrant colors and bold shapes.
+      Neobrutalism can also be used in print design, with an example
+      being a bold shape with a vibrant color fill applied to it."
+        />
+        <Card
+          title="Neo Brutallism"
+          description="Neobrutalism is an aesthetic characterized by high contrast
+      elements, bright colors, and bold shapes. It is often used to
+      make a statement, as it is meant to be eye-catching and stand
+      out to the viewer. It is also used in user interface and web
+      design, with features such as vibrant colors and bold shapes.
+      Neobrutalism can also be used in print design, with an example
+      being a bold shape with a vibrant color fill applied to it."
+        />
+        <Card
+          title="Neo Brutallism"
+          description="Neobrutalism is an aesthetic characterized by high contrast
+      elements, bright colors, and bold shapes. It is often used to
+      make a statement, as it is meant to be eye-catching and stand
+      out to the viewer. It is also used in user interface and web
+      design, with features such as vibrant colors and bold shapes.
+      Neobrutalism can also be used in print design, with an example
+      being a bold shape with a vibrant color fill applied to it."
+        />
+      </div>
     </main>
   );
 }
