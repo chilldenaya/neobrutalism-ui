057715deef54ffc62f30d628626278e7e0c13d92
denaya <chilldenaya@gmail.com>
2024-04-03 19:57:39 +0800

    feat: :sparkles: initialize project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold font-pen">Hello, world!</h1>
+      <p className="text-lg font-bold italic font-mono">
+        We are extremely proud and protective of our active & thriving
+        community.
+      </p>
+    </main>
+  );
+}
