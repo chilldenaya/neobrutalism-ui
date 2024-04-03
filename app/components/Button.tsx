57ff4f4e43278f2a8e72cf0f5aff718a1b4e5ad4
denaya <chilldenaya@gmail.com>
2024-04-03 22:48:22 +0800

    feat: :sparkles: show UI demo on homepage

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,14 +7,13 @@ interface ButtonProps {
   className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  onClick,
-  disabled,
-  children,
-  className,
-}) => {
+const Button: React.FC<ButtonProps> = ({ onClick, disabled, children }) => {
   return (
-    <button onClick={onClick} disabled={disabled} className={className}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className="w-[200px] hover-3d bg-lime-100 items-center rounded-lg m-2 p-2"
+    >
       {children}
     </button>
   );
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,18 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 interface CardProps {
   title: string;
   description: string;
+  image?: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description }) => (
+const Card: React.FC<CardProps> = ({ title, description, image }) => (
   <>
     <div className="bg-pink-100 w-80 h-full border-black border-2 rounded-md bg-white hover-3d">
       <a className="block cursor-pointer">
         <article className="w-full h-full">
           <div className="px-6 py-5 text-left h-full">
             <h1 className="text-3xl mb-4 font-mono">{title}</h1>
-            <p className="text-2xl mb-4 line-clamp-4 font-pen">{description}</p>
+            {image && (
+              <img
+                src={image}
+                alt="random"
+                className="w-full h-48 object-cover mb-4"
+                width={800}
+                height={600}
+              />
+            )}
+            <p className="text-xl mb-4 line-clamp-4 font-pen">{description}</p>
           </div>
         </article>
       </a>
